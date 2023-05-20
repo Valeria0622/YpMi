29daf85bde9bb4743883198d5b6d54efd6983539
gr604_davar <daktanlera@gmail.com>
2023-05-20 13:42:29 +0700

    изменения

--- YpMi/Program.cs
+++ YpMi/Program.cs
@@ -41,6 +41,21 @@ namespace YpMi
             b = double.Parse(Console.ReadLine());
             Console.WriteLine(' ');
 
+            if (q == "1")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Результат умножения = {0}", a * b);
+            }
+            if (q == "2")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Результат деления = {0}", a / b);
+            }
+            if (q == "3")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Результат сложения = {0}", a + b);
+            }
             if (q == "4")
             {
                 Console.ForegroundColor = ConsoleColor.Green;
